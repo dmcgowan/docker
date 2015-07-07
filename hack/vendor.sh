1183a31c2ad693f10ed41764281ae52a7c5756db
Derek McGowan <derek@mcgstyle.net>
2015-07-07 10:03:12 -0700

    Vendor notary
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -36,6 +36,11 @@ clone git github.com/hashicorp/consul v0.5.2
 # get distribution packages
 clone git github.com/docker/distribution 419bbc2da637d9b2a812be78ef8436df7caac70d
 
+clone git github.com/docker/notary eb52b6458677bc6fcce730a79888aaf7554dc49c
+clone git github.com/endophage/gotuf 5be7693587dc2f3c6b35fd1394fcc4e098b4f643
+clone git github.com/tent/canonical-json-go 96e4ba3a7613a1216cbd1badca4efe382adea337
+clone git github.com/agl/ed25519 d2b94fd789ea21d12fac1a4443dd3a3f79cda72c
+
 clone git github.com/opencontainers/runc v0.0.2 # libcontainer
 # libcontainer deps (see src/github.com/docker/libcontainer/update-vendor.sh)
 clone git github.com/coreos/go-systemd v2

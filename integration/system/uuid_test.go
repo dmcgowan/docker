aa439dfb529a2a85b949fb703a7ec8a07b07330a
Tibor Vass <tibor@docker.com>
2019-06-20 01:20:20 +0000

    distribution: remove v2 schema1 push
    Manifest v2 schema1 was deprecated in 4866f5139a1 and this commit
    removes the push code for v2 schema1.
    
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- integration/system/uuid_test.go
+++ integration/system/uuid_test.go
@@ -5,19 +5,17 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types/versions"
-	"github.com/google/uuid"
 	"gotest.tools/assert"
 	"gotest.tools/skip"
 )
 
 func TestUUIDGeneration(t *testing.T) {
-	skip.If(t, versions.LessThan(testEnv.DaemonAPIVersion(), "1.40"), "ID format changed")
+	skip.If(t, versions.LessThan(testEnv.DaemonAPIVersion(), "1.41"), "ID format changed")
 	defer setupTest(t)()
 
 	c := testEnv.APIClient()
 	info, err := c.Info(context.Background())
 	assert.NilError(t, err)
 
-	_, err = uuid.Parse(info.ID)
-	assert.NilError(t, err, info.ID)
+	assert.Assert(t, info.ID != "")
 }
